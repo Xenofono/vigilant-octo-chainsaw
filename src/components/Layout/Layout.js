86fbe304d92fac8d9874ce1b367da5109693d80f
Kristoffer Näsström <kristoffer.nasstrom@gmail.com>
2020-01-29 20:19:43 +0100

    layout placerholders added

---
+++ src/components/Layout/Layout.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const layout = props => (
+    <React.Fragment>
+        <div>
+            Toolbar, SideDrawer, Backdrop
+            
+        </div>
+        <main>
+            {props.children}
+        </main>
+    </React.Fragment>
+)
+
+export default layout;
