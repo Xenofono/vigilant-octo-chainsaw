1f28cf4215b30ed0feab52b2c5ec2296a8d331f6
Kristoffer Näsström <kristoffer.nasstrom@gmail.com>
2020-02-07 18:58:48 +0100

    merged finally

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -2,23 +2,10 @@ import React from "react";
 import "./Layout.css";
 
 const layout = props => (
-<<<<<<< HEAD
-    <React.Fragment>
-        <div>
-            Toolbar, SideDrawer, Backdrop
-
-        </div>
-        <main>
-            {props.children}
-        </main>
-    </React.Fragment>
-)
-=======
   <React.Fragment>
     <div>Toolbar, SideDrawer, Backdrop</div>
     <main className="Content">{props.children}</main>
   </React.Fragment>
 );
->>>>>>> d6f3103d2471c774f5e3b240a372bbaa96b1cc4c
 
 export default layout;
